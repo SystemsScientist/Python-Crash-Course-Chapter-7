25a9dc4be4c4a73e669f39816b3d111412d2cada
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-01-16 06:10:17 -0600

    Adding greeter2.py file

---
+++ Examples/greeter2.py
@@ -0,0 +1,12 @@
+# greeter2.py, Chapter 6, Python Crash Course
+# program uses a multi-line prompt to input
+# user input
+
+prompt = "If you tell us who you are, we can personalize the messages you see."
+prompt += "\nWhat is your first name? "
+
+name = input(prompt)
+print("\nHello, " + name + "!")
+
+
+
