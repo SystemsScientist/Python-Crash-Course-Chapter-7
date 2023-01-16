f7c111ac43e110476d91b3b8cba79906d08ee8fb
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-01-16 06:25:42 -0600

    Adding even_or_odd.py file

---
+++ Examples/even_or_odd.py
@@ -0,0 +1,13 @@
+# even_or_odd.py, Chapter 7, Python Crash Course
+# program determines if a number is odd or even
+
+number = input("Enter a number, and I'll tell you if it's even or odd: ")
+number = int(number)
+
+if number % 2 == 0:
+    print("\nThe number " + str(number) + " is even.")
+else:
+    print("\nThe number " + str(number) + " is odd.")
+
+
+
