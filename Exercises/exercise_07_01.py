f72c71e646cec12d8bb26f9d3608a7267152a200
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-01-16 06:32:32 -0600

    Adding exercise_07_01.py file

---
+++ Exercises/exercise_07_01.py
@@ -0,0 +1,12 @@
+# exercise_07_01.py, Chapter 7, Python Crash Course
+# 
+# Rental Car: Write a program that asks the user what
+# kind of rental car they would like. Print a message
+# about that car, such as "Let me see if I can find
+# you a Subaru."
+
+rental_car = input("What kind of rental car would you like? ")
+print("Let me see if I can find you a " + rental_car + ".")
+
+
+
