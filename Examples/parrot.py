36ae16a7f87e670766ac70d4ac6271360bd57d30
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-01-16 05:58:12 -0600

    Adding parrot.py file

---
+++ Examples/parrot.py
@@ -0,0 +1,9 @@
+# parrot.py, Chapter 7, Python Crash Course
+# program uses the input() function to return
+# the user input
+
+message = input("Tell me something, and I will repeat it back to you: ")
+print(message)
+
+
+
