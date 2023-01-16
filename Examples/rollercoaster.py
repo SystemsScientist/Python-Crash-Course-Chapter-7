b2329f83c9f574a4bf5eb9cac20ea0e8e9948a5e
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-01-16 06:17:40 -0600

    Adding rollercoaster.py file

---
+++ Examples/rollercoaster.py
@@ -0,0 +1,14 @@
+# rollercoaster.py, Chapter 7, Python Crash Course
+# program prompts user for height and then uses an
+# if-else block to compare
+
+height = input("How tall are you, in inches? ")
+height = int(height)
+
+if height >= 36:
+    print("\nYou're tall enough to ride!")
+else:
+    print("\nYou'll be able to ride when you're a little older.")
+
+
+
