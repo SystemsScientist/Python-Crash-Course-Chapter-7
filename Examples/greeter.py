a7ccb61ad797dcda31922a5eba5819a4ade0fa99
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-01-16 06:04:06 -0600

    Adding greeter.py file

---
+++ Examples/greeter.py
@@ -0,0 +1,10 @@
+# greeter.py, Chapter 7, Python Crash Course
+# program uses the input() function to return
+# user input
+
+# add a space at the end of your prompts
+name = input("Please enter your name: ")
+print("Hello, " + name + "!")
+
+
+
